e04e70b6e3e94072c876ee2c659f1a3c2dfe62dd
NGUYỄN TRẦN VINH <215748020110352@vinhuni.edu.vn>
2025-05-22 18:23:25 +0700

    vinh update22/5 6h

---
+++ app/src/main/java/com/example/docbaoapp/OnFetchDataListener.java
@@ -0,0 +1,11 @@
+package com.example.docbaoapp;
+
+import com.example.docbaoapp.Models.NewsHeadlines;
+
+import java.util.List;
+
+public interface OnFetchDataListener {
+    void onFetchData(List<NewsHeadlines> list, String message);
+    void onError(String message);
+
+}
