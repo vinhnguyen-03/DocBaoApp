ae7e758dd7e797a0378bee9e8475245b6f1dd0e9
NGUYỄN TRẦN VINH <215748020110352@vinhuni.edu.vn>
2025-05-23 08:43:28 +0700

    vinh update23/5 9h

--- app/src/main/java/com/example/docbaoapp/DetailsActivity.java
+++ app/src/main/java/com/example/docbaoapp/DetailsActivity.java
@@ -11,12 +11,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.docbaoapp.Models.NewsHeadlines;
+import com.squareup.picasso.Picasso;
 
 public class DetailsActivity extends AppCompatActivity {
 
     NewsHeadlines headlines;
     TextView txt_title, txt_author, txt_time, txt_detail, txt_content;
     ImageView img_news;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -24,6 +26,20 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
 
         txt_title = findViewById(R.id.txt_detail_title);
+        txt_author = findViewById(R.id.txt_detail_author);
+        txt_time = findViewById(R.id.txt_detail_time);
+        txt_detail = findViewById(R.id.txt_detail_detail);
+        txt_content = findViewById(R.id.txt_detail_content);
+        img_news = findViewById(R.id.img_detail_news);
+
         headlines = (NewsHeadlines) getIntent().getParcelableExtra("data");
+
+        txt_title.setText(headlines.getTitle());
+        txt_author.setText(headlines.getAuthor());
+        txt_time.setText(headlines.getPublishedAt());
+        txt_detail.setText(headlines.getDescription());
+        txt_content.setText(headlines.getContent());
+        Picasso.get().load(headlines.getUrlToImage()).into(img_news);
+
     }
 }
